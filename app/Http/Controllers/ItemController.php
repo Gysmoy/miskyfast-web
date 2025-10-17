050b157bb994b424a1eb867f0268abab7c9b020b
Manuel Gamboa <gamboapalominocarlosmanuel@gmail.com>
2025-10-17 03:09:18 -0500

    NTS

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -2,8 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use App\Models\Item;
+use App\Models\Restaurant;
 use Illuminate\Http\Request;
+use SoDe\Extend\Response;
 
 class ItemController extends BasicController
 {
@@ -12,13 +15,16 @@ class ItemController extends BasicController
     public $reactRootView = 'public';
     public $prefix4filter = 'items';
 
-    public function setPaginationInstance(Request $request, string $model)
+    public function byCategory(Request $request, Restaurant $restaurant, Category $category)
     {
-        $query = $model::select()
-            ->where('visible', true)
-            ->where('status', true)
-            ->where('restaurant_id', $request->restaurant);
-        if ($request->category) $query->where('category_id', $request->category);
-        return $query;
+        $response = Response::simpleTryCatch(function () use ($restaurant, $category) {
+            $query = Item::select()
+                ->where('visible', true)
+                ->where('status', true)
+                ->where('restaurant_id', $restaurant->id);
+            if ($category) $query->where('category_id', $category->id);
+            return $query->get();
+        });
+        return response($response->toArray(), $response->status);
     }
 }
