30de5779e9f565cf104931ffc070e1b8f028b6f9
Manuel Gamboa <gamboapalominocarlosmanuel@gmail.com>
2025-10-21 12:01:32 -0500

    NTS

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -3,9 +3,11 @@
 namespace App\Http\Controllers;
 
 use App\Models\Order;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response as HttpResponse;
 use Illuminate\Routing\ResponseFactory;
+use Illuminate\Support\Facades\Auth;
 use SoDe\Extend\Response;
 
 class OrderController extends BasicController
@@ -15,7 +17,8 @@ class OrderController extends BasicController
     public function save(Request $request): HttpResponse|ResponseFactory
     {
         $response = Response::simpleTryCatch(function () use ($request) {
-            dump($request);
+            $clientJpa = User::find(Auth::id());
+            return $clientJpa;
         });
         return response($response->toArray(), $response->status);
     }
